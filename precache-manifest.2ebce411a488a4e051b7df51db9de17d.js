4c9a7d06f93764f7546e1e39da42ac54455ff2e2
Kunal Shinde <kunalshinde373@gmail.com>
2020-06-19 14:37:12 +0530

    Updates

---
+++ precache-manifest.2ebce411a488a4e051b7df51db9de17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba879939ac766d2a736431a9966ee83",
+    "url": "/beezer-com/index.html"
+  },
+  {
+    "revision": "5eee24e8343ccbd5db5a",
+    "url": "/beezer-com/static/css/main.8e2047dc.chunk.css"
+  },
+  {
+    "revision": "d6a14bb339b366cfc8b9",
+    "url": "/beezer-com/static/js/2.2155f059.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beezer-com/static/js/2.2155f059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eee24e8343ccbd5db5a",
+    "url": "/beezer-com/static/js/main.fb262e71.chunk.js"
+  },
+  {
+    "revision": "09ff3d4e173e6df6330f",
+    "url": "/beezer-com/static/js/runtime-main.0db08983.js"
+  }
+]);
